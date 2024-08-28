fb5dc6e4886a7788a4bb9f8442a3de5331f45880
David <davidcastanera100@gmail.com>
2024-08-28 10:33:54 +0200

    mongoose Corregido

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose'
 
 const conectarDB = async () => {
   try {
-    const db = await mongoose.connect(
-      process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    )
+
+    const db = await mongoose.connect(process.env.MONGO_URI)
+
+    //las opciones de momento no
     const url = `${db.connection.host}:${db.connection.port}`
     console.log(`MongoDB conectado en: ${url}`)
   } catch (error) {
